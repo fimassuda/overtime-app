1ef3f1eadfc42905b2c289b24a517c9b2f2666ec
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-24 18:13:30 -0300

    Updated homepage flow for admin and users

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,6 +9,7 @@ module Admin
   def self.admin_types
     ['AdminUser']
   end
+  
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!, :authenticate_admin
 
