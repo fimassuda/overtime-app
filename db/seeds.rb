8dbe3ee07b7d9028bfa0f9059f735d2c5eda50d2
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-25 15:16:18 -0300

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,6 @@ AdminUser.create(email: "admin@test.com",
 
 puts "Admin User created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "100 posts have been created"
 
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
@@ -28,3 +23,9 @@ AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days
 
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 posts have been created"
