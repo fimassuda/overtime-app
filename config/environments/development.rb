094d726082bb7f7411d3a6a3b8da2fcc5ddddb19
Fillipe Massuda <fi.massuda@gmail.com>
2017-04-25 14:52:00 -0300

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   config.active_support.deprecation = :log
 
@@ -28,5 +25,12 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
